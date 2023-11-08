4224e2ee7674fbb7d897d14f8a281ed84335826d
sevicandra <sevicandra4@gmail.com>
2023-11-08 11:14:49 +0700

    fix ui

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,7 @@
     @vite('resources/css/app.css')
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="description" content="Monitoring Tagihan DJKN">
     <title>Monev</title>
     <link rel="shortcut icon" href="/img/monev.png" type=" image/x-icon">
     <style>
@@ -26,8 +26,8 @@
 
 <body class="bg-base-100 h-screen flex flex-col relative">
     <header class="navbar bg-neutral px-4 flex-none flex justify-between z-20">
-        <div class="flex-1 order-2 lg:order-1 flex justify-center lg:justify-start">
-            <a class="normal-case text-xl" href="/"><img src="/img/monev.png" width="25" height="25"></a>
+        <div class="flex-1 order-2 lg:order-1 flex justify-center lg:justify-start gap-1">
+            <a href="/"><img src="/img/monev.png" width="25" height="25" alt="logo"></a>
             <a class="normal-case text-xl text-neutral-content" href="/session/tahun-anggaran">&nbsp;MonevTagihan
                 {{ session()->get('tahun') }}</a>
         </div>
@@ -35,7 +35,7 @@
             <div class="dropdown lg:dropdown-end">
                 <label tabindex="0" class="btn btn-ghost btn-circle avatar">
                     <div class="w-10 rounded-full">
-                        <img src="{{ session()->get('gravatar') }}" />
+                        <img src="{{ session()->get('gravatar') }}" alt="profile"/>
                     </div>
                 </label>
                 <ul tabindex="0"
@@ -101,12 +101,12 @@
         </div>
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 <script>
     flatpickr("#myFlat", {
         dateFormat: "d-m-Y",
     });
-</script>
+</script> --}}
 {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script> --}}
 <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
     crossorigin="anonymous"></script>
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,156 +1,158 @@
 <div class="menu">
-    <li>
-        <details open>
-            <summary>Halaman Utama</summary>
-            <ul>
-                <li>
-                    <a href="/dashboard">
-                        Dashboard
-                    </a>
-                </li>
-                <li>
-                    <a href="/dashboard/unit">
-                        Realisasi Unit
-                    </a>
-                </li>
-                <li>
-                    <a href="/dashboard/ppk">
-                        Realisasi PPK
-                    </a>
-                </li>
-            </ul>
-        </details>
-    </li>
-    {{--  Akhir Halaman Utama  --}}
-    {{--  Pejabat Pembuat Komitmen  --}}
-    @canany(['PPK', 'Staf_PPK'], auth()->user()->id)
+    <menu>
         <li>
             <details open>
-                <summary>Pejabat Komitmen</summary>
+                <summary>Halaman Utama</summary>
                 <ul>
-                    @can('Staf_PPK', auth()->user()->id)
-                        <li>
-                            <a href="/tagihan">
-                                Data Tagihan
-                            </a>
-                        </li>
-                    @endcan
-                    {{-- <li>
-                        <a href="/register">
-                            Register Tagihan
-                        </a>
-                    </li> --}}
                     <li>
-                        <a href="/monitoring-tagihan">
-                            Arsip Tagihan
+                        <a href="/dashboard">
+                            Dashboard
                         </a>
                     </li>
-                    {{-- <li>
-                        <a href="/arsip-register">
-                            Arsip Register
-                        </a>
-                    </li> --}}
-                    {{--  Akhir Pejabat Pembuat Komitmen  --}}
                     <li>
-                        <details close>
-                            <summary>Referensi</summary>
-                            <ul>
-                                <li><a href="/referensi-rekening">Rekening</a></li>
-                                <li><a href="/rekanan">Rekanan</a></li>
-                            </ul>
-                        </details>                            
-                    </li>
-                </ul>
-            </details>
-        </li>
-    @endcanany
-
-    @canany(['PPSPM', 'Bendahara', 'Validator'], auth()->user()->id)
-        {{--  Bagian Keuangan  --}}
-        <li>
-            <details open>
-                <summary>Bagian Keuangan</summary>
-                <ul>
-                    @can('Validator', auth()->user()->id)
-                        <li>
-                            <a href="/verifikasi">
-                                Verifikasi
-                            </a>
-                        </li>
-                    @endcan
-                    @can('PPSPM', auth()->user()->id)
-                        <li>
-                            <a href="/ppspm">
-                                PPSPM
-                            </a>
-                        </li>
-                    @endcan
-                    @can('Bendahara', auth()->user()->id)
-                        <li>
-                            <a href="/payroll">
-                                Payroll
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/bendahara">
-                                Bendahara
-                            </a>
-                        </li>
-                    @endcan
-                    <li>
-                        <a href="/arsip">
-                            Arsip Tagihan
+                        <a href="/dashboard/unit">
+                            Realisasi Unit
                         </a>
                     </li>
-                    {{-- <li>
-                        <a href="/arsip-register">
-                            Arsip Register
-                        </a>
-                    </li> --}}
                     <li>
-                        <a href="/laporan-pajak">
-                            Laporan Pajak
+                        <a href="/dashboard/ppk">
+                            Realisasi PPK
                         </a>
                     </li>
                 </ul>
             </details>
         </li>
-        {{--  Akhir Bagian Keuangan  --}}
-    @endcanany
-    @canany(['admin_satker', 'sys_admin', 'KPA', 'Staf_KPA'], auth()->user()->id)
-        {{--  Admin  --}}
-        <li>
-            <details close>
-                <summary>Referensi</summary>
-                <ul>
-                    @canany(['sys_admin', 'admin_satker'], auth()->user()->id)
-                        <li><a href="/user">Users</a></li>
-                        <li><a href="/nomor">Nomor</a></li>
-                    @endcan
-                    @can('admin_satker', auth()->user()->id)
-                        <li><a href="/unit">Unit</a></li>
-                        <li><a href="/maping-ppk">Maping PPK</a></li>
-                        <li><a href="/maping-staf-ppk">Maping Staf PPK</a></li>
-                    @endcan
-
-                    @canany(['Staf_KPA', 'KPA'], auth()->user()->id)
-                        <li><a href="/pagu">Pagu</a></li>
-                    @endcanany
-                    @can('sys_admin', auth()->user()->id)
-                        <li><a href="/satker">Satuan Kerja</a></li>
-                        <li><a href="/dokumen">Jenis Dokumen</a></li>
-                        <li><a href="/tahun">Tahun Anggaran</a></li>
-                        <li><a href="/berkas">Berkas</a></li>
-                        <li><a href="/pph">PPH</a></li>
-                        <li><a href="/bulan">Bulan</a></li>
-                        <li><a href="/role">Role</a></li>
-                        <li><a href="/referensi/objek-pajak">Objek Pajak</a></li>
-                    @endcan
-                </ul>
-            </details>
-        </li>
-        {{--  Akhir Admin  --}}
-    @endcanany
-    <li class="">Version : 2.3.0</li>
+        {{--  Akhir Halaman Utama  --}}
+        {{--  Pejabat Pembuat Komitmen  --}}
+        @canany(['PPK', 'Staf_PPK'], auth()->user()->id)
+            <li>
+                <details open>
+                    <summary>Pejabat Komitmen</summary>
+                    <ul>
+                        @can('Staf_PPK', auth()->user()->id)
+                            <li>
+                                <a href="/tagihan">
+                                    Data Tagihan
+                                </a>
+                            </li>
+                        @endcan
+                        {{-- <li>
+                            <a href="/register">
+                                Register Tagihan
+                            </a>
+                        </li> --}}
+                        <li>
+                            <a href="/monitoring-tagihan">
+                                Arsip Tagihan
+                            </a>
+                        </li>
+                        {{-- <li>
+                            <a href="/arsip-register">
+                                Arsip Register
+                            </a>
+                        </li> --}}
+                        {{--  Akhir Pejabat Pembuat Komitmen  --}}
+                        <li>
+                            <details close>
+                                <summary>Referensi</summary>
+                                <ul>
+                                    <li><a href="/referensi-rekening">Rekening</a></li>
+                                    <li><a href="/rekanan">Rekanan</a></li>
+                                </ul>
+                            </details>                            
+                        </li>
+                    </ul>
+                </details>
+            </li>
+        @endcanany
+    
+        @canany(['PPSPM', 'Bendahara', 'Validator'], auth()->user()->id)
+            {{--  Bagian Keuangan  --}}
+            <li>
+                <details open>
+                    <summary>Bagian Keuangan</summary>
+                    <ul>
+                        @can('Validator', auth()->user()->id)
+                            <li>
+                                <a href="/verifikasi">
+                                    Verifikasi
+                                </a>
+                            </li>
+                        @endcan
+                        @can('PPSPM', auth()->user()->id)
+                            <li>
+                                <a href="/ppspm">
+                                    PPSPM
+                                </a>
+                            </li>
+                        @endcan
+                        @can('Bendahara', auth()->user()->id)
+                            <li>
+                                <a href="/payroll">
+                                    Payroll
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/bendahara">
+                                    Bendahara
+                                </a>
+                            </li>
+                        @endcan
+                        <li>
+                            <a href="/arsip">
+                                Arsip Tagihan
+                            </a>
+                        </li>
+                        {{-- <li>
+                            <a href="/arsip-register">
+                                Arsip Register
+                            </a>
+                        </li> --}}
+                        <li>
+                            <a href="/laporan-pajak">
+                                Laporan Pajak
+                            </a>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+            {{--  Akhir Bagian Keuangan  --}}
+        @endcanany
+        @canany(['admin_satker', 'sys_admin', 'KPA', 'Staf_KPA'], auth()->user()->id)
+            {{--  Admin  --}}
+            <li>
+                <details close>
+                    <summary>Referensi</summary>
+                    <ul>
+                        @canany(['sys_admin', 'admin_satker'], auth()->user()->id)
+                            <li><a href="/user">Users</a></li>
+                            <li><a href="/nomor">Nomor</a></li>
+                        @endcan
+                        @can('admin_satker', auth()->user()->id)
+                            <li><a href="/unit">Unit</a></li>
+                            <li><a href="/maping-ppk">Maping PPK</a></li>
+                            <li><a href="/maping-staf-ppk">Maping Staf PPK</a></li>
+                        @endcan
+    
+                        @canany(['Staf_KPA', 'KPA'], auth()->user()->id)
+                            <li><a href="/pagu">Pagu</a></li>
+                        @endcanany
+                        @can('sys_admin', auth()->user()->id)
+                            <li><a href="/satker">Satuan Kerja</a></li>
+                            <li><a href="/dokumen">Jenis Dokumen</a></li>
+                            <li><a href="/tahun">Tahun Anggaran</a></li>
+                            <li><a href="/berkas">Berkas</a></li>
+                            <li><a href="/pph">PPH</a></li>
+                            <li><a href="/bulan">Bulan</a></li>
+                            <li><a href="/role">Role</a></li>
+                            <li><a href="/referensi/objek-pajak">Objek Pajak</a></li>
+                        @endcan
+                    </ul>
+                </details>
+            </li>
+            {{--  Akhir Admin  --}}
+        @endcanany
+        <li class="">Version : 2.3.0</li>
+    </menu>
 </div>
 {{--  Halaman Utama  --}}
